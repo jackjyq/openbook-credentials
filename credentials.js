bae3b1dcdd2f97d3d95977831b463438b1740ed2
Jack Jiang <jackjyq@outlook.com>
2025-01-01 09:38:41 +0800

    improve search

--- credentials.js
+++ credentials.js
@@ -1,28 +1,28 @@
-/* This file contains sample credentials. Please modify as needed.*/
+/* This file contains sample credentials. Please modify as needed.
 
-const credentials = () => {
-  return [
-    {
-      // sample: typical
-      item: "Company Email",
-      url: "https://mail.company.com",
-      username: "user",
-      password: "monkey",
-    },
-    {
-      // sample: minimal
-      item: "iPhone",
-      password: "123456",
-    },
-    {
-      // sample: informative
-      item: "AWS Account",
-      url: "https://www.aws.com",
-      username: "example",
-      password: "orange",
-      tags: ["aws", "production"],
-      additional: "1234567890",
-      extra: "abcdefg",
-    },
-  ];
-};
+you may use https://vscode.dev/
+*/
+
+const getCredentials = () => [
+  {
+    // sample: typical
+    item: "Company Email",
+    url: "https://mail.company.com",
+    username: "user",
+    password: "make money",
+  },
+  {
+    // sample: minimal
+    item: "iPhone",
+    password: "123456",
+  },
+  {
+    // sample: informative
+    item: " Cloud Account",
+    url: "https://www.cloud.com",
+    username: "example",
+    password: "orange juice",
+    tags: ["AWS  ", "production"],
+    additional: "admin",
+  },
+];
